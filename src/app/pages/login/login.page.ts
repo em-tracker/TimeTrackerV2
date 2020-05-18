ee97571cbc3a0d0370dab6a60c617c62f4709780
Dennis Domingo <ddomingo@agsx.net>
2020-05-18 08:35:45 +0800

    added blocker in new login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -9,12 +10,26 @@ export class LoginPage implements OnInit {
   email: string;
   password: string;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private alertCtrl: AlertController
+  ) {}
 
   ngOnInit() {}
 
   login() {
-    const { email, password } = this;
-    this.authService.signIn(email + '@gmail.com', password);
+    // const { email, password } = this;
+    // this.authService.signIn(email + '@gmail.com', password);
+    this.showAlert();
+  }
+
+  async showAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Login Warning',
+      message:
+        'This feature is under development thank you for registering on the new TimeTracker',
+      buttons: ['OK'],
+    });
+    await alert.present();
   }
 }
